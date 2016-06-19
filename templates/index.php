86e7e1e22f19a4fd1cb6456210180b166c77f445
p4scu41 <pereznango@gmail.com>
2016-06-19 08:33:15 -0500

    Update page title

--- templates/index.php
+++ templates/index.php
@@ -3,7 +3,7 @@
     <head>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
-        <title>Carrito de Compras</title>
+        <title>Carrito de Compras en Angular</title>
 
         <!-- Bootstrap -->
         <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css">
